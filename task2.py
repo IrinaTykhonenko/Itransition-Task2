d7fe139decb722d2d57a9ab5e77d7a9af345068b
IrinaTykhonenko <tykhonenko.irina@gmail.com>
2022-02-08 15:42:28 +0300

    task2 complete

---
+++ task2.py
@@ -0,0 +1,18 @@
+
+import sha3
+import os
+
+path = os.path.join(os.getcwd(), 'task2')
+s = []
+for filename in os.listdir(path):
+   with open(os.path.join(path, filename), 'rb') as f:
+       s.append(sha3.sha3_256(f.read()).hexdigest())
+s = sorted(s)
+print(s)
+string = ''.join(s)
+string += "tykhonenko.irina@gmail.com"
+print(sha3.sha3_256(str.encode(string)).hexdigest())
+
+
+
+
